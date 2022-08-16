848eb165976ebc8c07783978fee9f26a6975e2d4
rasulshirinboev <rasulboev9@gmail.com>
2022-08-16 14:53:34 -0400

    fourth commit

--- m3-lambda/src/main/java/com/cydeo/FunctionInterface.java
+++ m3-lambda/src/main/java/com/cydeo/FunctionInterface.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionInterface {
@@ -12,11 +15,21 @@ public class FunctionInterface {
             }
         };
 
-        //************PREDICATE*******************//
+        System.out.println("************PREDICATE*******************");
         Predicate<Integer> lesserThan = i -> i<18;
         System.out.println(lesserThan.test(20));
 
+        System.out.println("************Consumer*******************");
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(10);
 
+        System.out.println("************BI  Consumer*******************");
+        BiConsumer<Integer, Integer> addTwo = (x,y) ->System.out.println(x+y);
+        addTwo.accept(1,2);
+
+        System.out.println("************Function*******************");
+        Function<String, String> fun = s -> "Hello " + s;
+        System.out.println(fun.apply("Cydeo"));
 
 
 
