f42f07ec98805d2e543390b7c64ed92094735130
rasulshirinboev <rasulboev9@gmail.com>
2022-08-16 15:50:42 -0400

    sixth commit

---
+++ m3-lambda/src/main/java/com/cydeo/task/User.java
@@ -0,0 +1,16 @@
+package com.cydeo.task;
+
+import lombok.*;
+
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String firstName;
+    private String lastNAme;
+    private int age;
+}
