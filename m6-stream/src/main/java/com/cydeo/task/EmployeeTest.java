ea14e4e6fc0c073e1c1a9539719fbf266acf6b18
rasulshirinboev <rasulboev9@gmail.com>
2022-08-19 16:53:57 -0400

    m6-streams

--- m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
+++ m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
@@ -1,9 +1,28 @@
 package com.cydeo.task;
 
+import java.util.List;
+
 public class EmployeeTest {
     public static void main(String[] args) {
 
         //Print all emails
+        System.out.println("Print All Emails");
+        EmployeeData.readAll()
+       //         .map(employee -> employee.getEmpMail())
+                .map(Employee::getEmpMail)
+
+                .forEach(System.out::println);
+
+        System.out.println("Print All Phone Numbers");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+        System.out.println("Print All Phone Numbers with Double Colon");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
 
 
     }
